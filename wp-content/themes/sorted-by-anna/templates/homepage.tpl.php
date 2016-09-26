b913eb47c1767ff17b92883d5eea7fdade0e3017
Patrick Johnson <im@pbj.me>
2016-09-25 22:51:36 -0400

    include homepage template

---
+++ wp-content/themes/sorted-by-anna/templates/homepage.tpl.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+ * Template Name: Home Page Template
+ */
+
+get_header();
+?>
+
+<h1>Homepage</h1>
+
+<div class="hero">
+  video background
+
+  CTA
+
+</div>
+
+<div class="services">
+  
+  services list
+</div>
+
+<div class="cta">
+  
+  book now
+</div>
+
+<div class="testimonials">
+  testimonials list
+</div>
+
+<?php 
+
+get_footer();
+
+?>
