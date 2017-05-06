12d4337e3b4b884de490e87a85e33a49b989f1e0
Patrick Johnson <im@pbj.me>
2017-05-06 14:00:55 -0400

    setup how it works section

--- wp-content/themes/sorted-by-anna/index.php
+++ wp-content/themes/sorted-by-anna/index.php
@@ -38,30 +38,21 @@ the_post(); ?>
 </section>
 
 <section>
-    <div class="testimonial-slider" data-js-component="postSlider">
-        <div class="testimonial-slider__slide">
-            <div class="testimonial-slider__content">
-                Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+    <?php the_partial('testimonials'); ?>
+</section>
 
-                <span class="testimonial-slider__client">Lauren Z.</span>
-            </div>
-        </div>
+<section>
+    <?php the_partial('section-title', [
+        'title' => 'How It Works'
+    ]); ?>
 
-        <div class="testimonial-slider__slide">
-            <div class="testimonial-slider__content">
-                “Anna did the best job possible. The space is finally usable and I don’t feel like a hot mess. Thank you!”
-
-                <span class="testimonial-slider__client">Lauren Z.</span>
-            </div>
-        </div>
-
-        <div class="testimonial-slider__slide">
-            <div class="testimonial-slider__content">
-                “Anna did the best job possible. The space is finally usable and I don’t feel like a hot mess. Thank you!”
-
-                <span class="testimonial-slider__client">Lauren Z.</span>
-            </div>
-        </div>
+    <div class="how-it-works">
+        <ol class="how-it-works__list">
+            <li class="how-it-works__list-item">Book your free consultation to discuss your space and challenges.</li>
+            <li class="how-it-works__list-item">List Item 2</li>
+            <li class="how-it-works__list-item">List Item 3</li>
+            <li class="how-it-works__list-item">List Item 4</li>
+        </ol>
     </div>
 </section>
 
---
+++ wp-content/themes/sorted-by-anna/partials/testimonials.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Testimonials Slider
+ * @var string
+ */
+?>
+
+<div class="testimonial-slider" data-js-component="postSlider">
+      <div class="testimonial-slider__slide">
+          <div class="testimonial-slider__content">
+              Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+              <span class="testimonial-slider__client">Lauren Z.</span>
+          </div>
+      </div>
+
+      <div class="testimonial-slider__slide">
+          <div class="testimonial-slider__content">
+              “Anna did the best job possible. The space is finally usable and I don’t feel like a hot mess. Thank you!”
+
+              <span class="testimonial-slider__client">Lauren Z.</span>
+          </div>
+      </div>
+
+      <div class="testimonial-slider__slide">
+          <div class="testimonial-slider__content">
+              “Anna did the best job possible. The space is finally usable and I don’t feel like a hot mess. Thank you!”
+
+              <span class="testimonial-slider__client">Lauren Z.</span>
+          </div>
+      </div>
+  </div>
