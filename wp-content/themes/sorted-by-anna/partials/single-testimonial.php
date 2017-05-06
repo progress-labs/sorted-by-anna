23b6b37aa1fc085bba429bd880a425c506e979b0
Patrick Johnson <im@pbj.me>
2017-05-06 17:04:25 -0400

    cleaning up

---
+++ wp-content/themes/sorted-by-anna/partials/single-testimonial.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Single Testimonial
+ */
+
+?>
+
+<div class="single-testimonial">
+    <div class="single-testimonial__slide">
+        <div class="single-testimonial__content">
+            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+            <span class="single-testimonial__client">Lauren Z.</span>
+        </div>
+    </div>
+
+</div>
--- wp-content/themes/sorted-by-anna/single-portfolio.php
+++ wp-content/themes/sorted-by-anna/single-portfolio.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once( get_template_directory() . '/functions/lib/data/class-post-view-model.php' );
 include_once( get_template_directory() . '/functions/view-models/class-portfolio-view-model.php' );
 
@@ -19,10 +19,10 @@ $related_portfolios = new WP_Query( $portfolio_args );
 <main class="single-project">
     <div class="page-container">
 
-    <?php while (have_posts()): the_post(); 
+    <?php while (have_posts()): the_post();
 
     $project = new Portfolio_View_Model($post); ?>
-    
+
     <div class="content-wrap">
         <div class="single-project__header">
             <h1 class="project-title">
@@ -33,19 +33,19 @@ $related_portfolios = new WP_Query( $portfolio_args );
                 <?php echo date( 'F jS, Y', strtotime( $post->post_date) );?>
             </span>
         </div>
-        
+
         <?php the_content(); ?>
-            
+
         <?php if ( $project->gallery() ) : ?>
             <div class="gallery" data-js-component="gallery">
                 <?php foreach ($project->gallery() as $gallery) : ?>
                     <a href="<?php echo $gallery['image_lg']; ?>"
-                    class="gallery__item" 
+                    class="gallery__item"
                     data-featherlight="image">
                         <div class="gallery__img-wrap">
-                            <img class="gallery__img" src="<?php echo $gallery['image']; ?>" alt="">     
+                            <img class="gallery__img" src="<?php echo $gallery['image']; ?>" alt="">
                         </div>
-                        
+
                     </a>
 
 
@@ -53,11 +53,13 @@ $related_portfolios = new WP_Query( $portfolio_args );
             </div>
         <?php endif; ?>
     </div>
-      
-      
+
+
     <?php endwhile; ?>
 
     </div>
+
+
 
 
     <div class="related-projects">
@@ -66,11 +68,13 @@ $related_portfolios = new WP_Query( $portfolio_args );
             <?php the_partial( 'related-project', array(
                 'project' => $related_project
             )); ?>
-           
-            
+
+
         <?php endwhile; ?>
     </div>
-    
+
+    <?php var_dump( get_field('single_testimonial') ); ?>
+
 
     <?php the_partial('consultation-cta'); ?>
 </main>
