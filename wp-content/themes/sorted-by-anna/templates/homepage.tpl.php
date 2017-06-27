437b57b324921404ee2aa5de8f2e717ae8a0fa35
Patrick Johnson <im@pbj.me>
2017-06-26 23:42:27 -0400

    make feature project into partial

--- wp-content/themes/sorted-by-anna/templates/homepage.tpl.php
+++ wp-content/themes/sorted-by-anna/templates/homepage.tpl.php
@@ -73,28 +73,11 @@ the_partial('nav');
 </div>
 
 
-<?php foreach ($featured_projects as $project) : ?>
-    <div class="featured-project">
-        <div class="featured-project__primary">
-            <div class="featured-project__media-wrap">
-                <img class="featured-project__image" src="<?php echo get_the_post_thumbnail_url( $project['id'] );?>" alt="">
-
-            </div>
-            <div class="show-at-small">
-                <a class="featured-project__cat-cta" href="<?php echo get_term_link( $project['term']['id'] ); ?>">View All <?php echo $project['term']['name']; ?> ⟶</a>
-            </div>
-        </div>
-
-        <a href="<?php echo get_the_permalink( $project['id'] );?>" class="featured-project__content">
-            <?php echo $project['content']; ?>
-            <span  class="featured-project__cta">View Project ⟶</span>
-        </a>
-
-        <div class="hide-at-small">
-            <a class="featured-project__cta featured-project__cta--mobile" href="<?php echo get_term_link( $project['term']['id'] ); ?>">View All <?php echo $project['term']['name']; ?> ⟶</a>
-        </div>
-    </div>
-<?php endforeach; ?>
+<?php foreach ($featured_projects as $project) {
+    the_partial('featured-project', [
+        'project' => $project
+    ]);
+} ?>
 
 
 
