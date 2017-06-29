24ca2df2ad8e8fc8b191655b9edd20384f65d2cf
Patrick Johnson <im@pbj.me>
2017-06-28 21:51:27 -0400

    remove unused partials. setup basic services page

--- wp-content/themes/sorted-by-anna/partials/calendly-embed.php
+++ wp-content/themes/sorted-by-anna/partials/calendly-embed.php
@@ -1,10 +1,17 @@
-<?php 
+<?php
 /**
  * Calendly Embed
  */
 ?>
 
-<div class="calendly">
-  <h2>Book A Consultation Now!</h2>
-  <iframe src="<?php echo get_field('calendly_url', 'option'); ?>" style="width:100%; height:500px;" frameBorder="0" scrolling="yes"></iframe>
+<div class="page-section">
+    <div class="backdrop">
+        <div class="page-container calendly">
+            <?php the_partial( 'section-title', [
+                'title' => 'Book A Free Consultation Now'
+            ]); ?>
+          <iframe src="<?php echo get_field('calendly_url', 'option'); ?>" style="width:100%; height:500px;" frameBorder="0" scrolling="yes"></iframe>
+        </div>
+    </div>
+
 </div>
--- wp-content/themes/sorted-by-anna/templates/service-details.tpl.php
+++ wp-content/themes/sorted-by-anna/templates/service-details.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Template Name: Service Details
@@ -7,107 +7,100 @@
 include_once( get_template_directory() . '/functions/lib/data/class-post-view-model.php' );
 include_once( get_template_directory() . '/functions/view-models/class-service-view-model.php' );
 
-$service_objects = get_field( 'services' );
-$portfolio_objects = get_field( 'portfolio_items' );
-$testimonial_objects = get_field( 'testimonials_slider' );
+$featured_projects = get_field( 'featured_projects' );
+$testimonials = get_field( 'testimonials_slider' );
+
+var_dump($testimonials);
 
 get_header();
 
-the_partial('nav');
 ?>
 
+
+<?php
+    the_partial('nav');
+    the_partial( 'page-hero', array(
+      'title' => get_the_title()
+    ));
+?>
 <div class="page-container">
-  <?php the_partial( 'page-hero', array(
-    'title' => get_the_title() 
-  )); ?>
 
-  <div class="content-wrap">
     <div class="page-content">
+      <?php if ( have_posts() ) {
+              while ( have_posts() ) {
+                the_post();
 
-      <?php the_partial('callout', [
-        'text' => get_field('callout')
-      ]) ?>
-
-      <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-          
-          <div class="grid">
-            <div class="two-third main-content">
-              <?php the_content(); ?>
-
-              <?php the_partial('image-cluster') ?>
-            </div>
-
-            <aside class="content-aside one-third has-left-border">
-
-              <?php the_partial('how-to-list', [
-                'title' => 'How It Works',
-                'list'  => get_field('how_to_steps')
-              ]) ?>
-            </aside>
-            
-          </div>
-
-        <?php endwhile; ?> 
-      <?php endif; ?>
-      
-      <h2 class="services-grid__title">Services Include:</h2>
-      <div class="services-grid">
-
-
-        <?php foreach ($service_objects as $object) :  ?>
-          <div class="services-grid__col">
-
-            <div class="services-card">
-
-                <?php if ( get_field('gallery_images', $object->ID) ) : 
-                  $img = get_field('gallery_images', $object->ID)[0]['image']['sizes']['medium'];
-                ?>
-                  <div class="services-card__wrap">
-                    <img src="<?php echo $img; ?>" alt="">                  
-                  </div>
-
-                <?php elseif ( has_post_thumbnail( $object->ID ) ) : ?>
-                  
-                  <div class="services-card__wrap">
-                    <?php echo get_the_post_thumbnail( $object->ID, 'medium' ); ?>
-                  </div>
-
-                <?php endif; ?>
-
-                <h2 class="services-card__title"><?php echo get_the_title( $object->ID ); ?></h2>
-                <p><?php echo get_the_content( $object->ID ); ?></p>
-
-            </div>
-          </div>
-        <?php endforeach; ?>
-
-      </div>
-
-      <?php if( get_field('display_calendar') ) : ?>
-        
-        <?php the_partial('calendly-embed'); ?>
-
-      <?php endif; ?>
-
-      <?php if ( $testimonial_objects ) {
-        the_partial('post-slider', [
-          'title'         => 'Testimonials', 
-          'slides' => $testimonial_objects
-        ]);
-      }?>
-
-      <?php the_partial('bio'); ?>
+                the_content();
+              }
+          }
+        ?>
     </div>
-  </div>
+</div>
 
+<?php if( get_field('display_calendar') ) : ?>
 
-  
-  
+  <?php the_partial('calendly-embed'); ?>
+
+<?php endif; ?>
+
+<?php if ( $testimonials ) : ?>
+<div class="page-section">
+      <?php the_partial( 'testimonials' ); ?>
+</div>
+<?php endif; ?>
+
+<div class="page-section">
+    <div class="color-block">
+        <div class="color-block__image">
+            <img src="http://placehold.it/500x600" alt="">
+        </div>
+        <div class="color-block__inner">
+
+            <div class="color-block__content">
+                <h3 class="color-block__title">This is a title</h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum incidunt, cumque dolor? Doloribus libero quasi voluptatem enim magni, ab ipsa qui sunt aliquam recusandae accusamus repellat, error dignissimos! Eius, temporibus.
+            </div>
+        </div>
+    </div>
+    <div class="color-block">
+        <div class="color-block__image">
+            <img src="http://placehold.it/500x600" alt="">
+        </div>
+        <div class="color-block__inner">
+
+            <div class="color-block__content">
+                <h3 class="color-block__title">This is a title</h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum incidunt, cumque dolor? Doloribus libero quasi voluptatem enim magni, ab ipsa qui sunt aliquam recusandae accusamus repellat, error dignissimos! Eius, temporibus.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="backdrop">
+    <?php the_partial( 'section-title', [
+        'title' => 'Recent Projects'
+    ]); ?>
+
+    <div class="grid grid-3-up">
+        <?php foreach ( $featured_projects as $project ) : ?>
+            <div class="col">
+                <?php the_partial('post-preview', [
+                    'url'    => get_the_permalink( $project->ID ),
+                    'title'  => get_the_title( $project->ID ),
+                    'img'    => 'http://placehold.it/400x300',
+                    'date'   => get_the_date( 'F j, Y' ),
+                    'category' => false,
+                    'excerpt' => false,
+                    'content' => $project->post_content,
+                    'read_more' => true
+                ]); ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
 
 </div>
 
-<?php 
+<?php
 
 get_footer();
 
